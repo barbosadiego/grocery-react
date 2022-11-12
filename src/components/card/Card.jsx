da786555cf7c516f2e5d9da0cd3c5636f9501e95
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-12 15:43:00 -0300

    create card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import './Card.scss';
+
+export default function Card(item) {
+  return (
+    <div className="item">
+      <div className="discount">{item.discountPercentage}%</div>
+      <span className="brand">{item.brand}</span>
+      <img src={item.thumbnail} alt={item.title} width={90} height={90} />
+      <span className="price">US$ {item.price}</span>
+      <Link to="/shop">
+        <button className="btn">Buy</button>
+      </Link>
+    </div>
+  );
+}
