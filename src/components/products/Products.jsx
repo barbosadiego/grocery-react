718f37b3db0c53f5f3941f067cd8d6130ee86424
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-12 14:31:42 -0300

    add products component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Products.scss';
 
 const Products = () => {
-  return <section className="products container">Products</section>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const data = await fetch(
+        'https://dummyjson.com/products/category/groceries',
+      );
+      const res = await data.json();
+
+      if (res.products) {
+        setProducts(res.products);
+      }
+    };
+
+    getProducts();
+  }, []);
+
+  return (
+    <section className="products container">
+      <div className="products__list">
+        {products.map((item) => {
+          return (
+            <div className="products__item" key={item.id}>
+              <div className="discount">{item.discountPercentage}%</div>
+              {/* <h3 className="title">{item.title}</h3> */}
+              <span className="brand">{item.brand}</span>
+              <img
+                src={item.thumbnail}
+                alt={item.title}
+                width={90}
+                height={90}
+              />
+              <span className="price">US$ {item.price}</span>
+              <Link to="/shop">
+                <button className="btn">Buy</button>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Products;
