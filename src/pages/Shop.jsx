f0e72c20b28a8510bd26342447caabfe43aa46f7
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-12 15:57:22 -0300

    add shop component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,38 @@
-import Products from '../components/products/Products';
+import { useEffect, useState } from 'react';
+import Card from '../components/card/Card';
+import useApi from '../hooks/useApi';
+
+import './Shop.scss';
 
 const Shop = () => {
-  return <div className="container">shop</div>;
+  const api = useApi();
+  const [items, setItems] = useState([]);
+
+  async function getProducts() {
+    const data = await api.getItems();
+    setItems(data);
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <section className="shop">
+        <h1>shop</h1>
+        <div className="items container">
+          {items.map((item) => {
+            return (
+              <div className="item" key={item.id}>
+                <Card item={item} />
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Shop;
