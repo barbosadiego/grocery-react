c0c64d74a21f189ebfdeeb7b5ee125f029940e8c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-12 15:43:23 -0300

    update products component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import useApi from '../../hooks/useApi';
+import Card from '../card/Card';
 import './Products.scss';
 
 const Products = () => {
@@ -16,27 +16,13 @@ const Products = () => {
     getProducts();
   }, []);
 
+  console.log(products);
+
   return (
     <section className="products container">
       <div className="products__list">
         {products.map((item) => {
-          return (
-            <div className="products__item" key={item.id}>
-              <div className="discount">{item.discountPercentage}%</div>
-              {/* <h3 className="title">{item.title}</h3> */}
-              <span className="brand">{item.brand}</span>
-              <img
-                src={item.thumbnail}
-                alt={item.title}
-                width={90}
-                height={90}
-              />
-              <span className="price">US$ {item.price}</span>
-              <Link to="/shop">
-                <button className="btn">Buy</button>
-              </Link>
-            </div>
-          );
+          return <Card item={item} key={item.id} />;
         })}
       </div>
     </section>
