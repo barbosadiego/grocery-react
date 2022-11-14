3682d64d273d0280831f2ba5324d9b8e0fba3da7
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 14:35:08 -0300

    add global context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { StrictMode } from 'react';
 import ReactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { GlobalProvider } from './contexts/GlobalContext';
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GlobalProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GlobalProvider>
   </StrictMode>,
 );
