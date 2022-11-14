be3fb19aad7a07fa5b979e8cc15afa6877cfce61
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 16:36:00 -0300

    add context and update button in cart

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,28 @@
+import { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { GlobalContext } from '../../contexts/GlobalContext';
 
 import './Card.scss';
 
-export default function Card({ item }) {
+export default function Card({ item, cartBtn = false, linkBtn = true }) {
+  const { addItemToCart } = useContext(GlobalContext);
+
   return (
     <div className="item">
       <div className="discount">{item.discountPercentage}%</div>
       <span className="brand">{item.brand}</span>
       <img src={item.thumbnail} alt={item.title} width={90} height={90} />
       <span className="price">US$ {item.price}</span>
-      <Link to="/shop">
-        <button className="btn">Buy</button>
-      </Link>
+      {linkBtn && (
+        <Link to="/shop">
+          <button className="btn">buy</button>
+        </Link>
+      )}
+      {cartBtn && (
+        <button className="btn" onClick={() => addItemToCart(item)}>
+          add to cart
+        </button>
+      )}
     </div>
   );
 }
