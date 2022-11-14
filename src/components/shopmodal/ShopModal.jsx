3b37051f052f72ca89385534f892a9cd23f4489e
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 16:38:05 -0300

    add methods and shop modal structure

--- src/components/shopmodal/ShopModal.jsx
+++ src/components/shopmodal/ShopModal.jsx
@@ -1,13 +1,38 @@
 import { useContext } from 'react';
 import { GlobalContext } from '../../contexts/GlobalContext';
+
 import './ShopModal.scss';
 
 export default function ShopModal() {
-  const { state } = useContext(GlobalContext);
+  const { state, handleShopModal, handleAppActive } = useContext(GlobalContext);
+
+  function handleClose() {
+    handleAppActive();
+    handleShopModal();
+  }
 
   return (
     <div className={`shop-modal ${state.isShopModalActive ? 'active' : ''}`}>
-      shop modal
+      <button className="close btn" onClick={handleClose}>
+        X
+      </button>
+      <div className="content">
+        {state.shopCart.map((item, index) => (
+          <div className="item" key={index}>
+            <img
+              className="img"
+              src={item.thumbnail}
+              alt={item.title}
+              width={50}
+              height={50}
+            />
+            <p className="title">{item.title}</p>
+          </div>
+        ))}
+        {state.shopCart.length === 0 && (
+          <p className="empty">No items in cart</p>
+        )}
+      </div>
     </div>
   );
 }
