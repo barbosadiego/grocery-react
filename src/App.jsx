43cd8a3d1d166e911be0e1bbc29ddadecb492e88
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 16:35:19 -0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { useContext } from 'react';
 import { GlobalContext } from './contexts/GlobalContext';
 
 import Header from './components/header/Header';
-
 import Home from './pages/Home';
 import About from './pages/About';
 import Shop from './pages/Shop';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
-
-import './App.scss';
 import Footer from './components/footer/Footer';
 import ShopModal from './components/shopmodal/ShopModal';
+import CartPage from './pages/CartPage';
+
+import './App.scss';
 
 const App = () => {
   const { state } = useContext(GlobalContext);
@@ -29,6 +29,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<CartPage />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
