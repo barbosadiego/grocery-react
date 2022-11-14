0169069ad5930fa5087e59259ff67ca97d68c60b
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 14:37:37 -0300

    add handle click method

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,37 +1,44 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { GlobalContext } from '../../contexts/GlobalContext';
 import { MenuContext } from '../../contexts/MenuContext';
 import './Menu.scss';
 
 const Menu = () => {
   const { showMenu, toggleMenu } = useContext(MenuContext);
+  const { handleAppActive } = useContext(GlobalContext);
+
+  function handleClick() {
+    toggleMenu();
+    handleAppActive();
+  }
 
   return (
     <nav className={`nav container ${showMenu ? 'active' : ''}`}>
       <div className="btn">browse all categories</div>
       <ul className="nav__links">
         <li>
-          <Link onClick={toggleMenu} to="/">
+          <Link onClick={handleClick} to="/">
             home
           </Link>
         </li>
         <li>
-          <Link onClick={toggleMenu} to="/about">
+          <Link onClick={handleClick} to="/about">
             about
           </Link>
         </li>
         <li>
-          <Link onClick={toggleMenu} to="/shop">
+          <Link onClick={handleClick} to="/shop">
             shop
           </Link>
         </li>
         <li>
-          <Link onClick={toggleMenu} to="/blog">
+          <Link onClick={handleClick} to="/blog">
             blog
           </Link>
         </li>
         <li>
-          <Link onClick={toggleMenu} to="/contact">
+          <Link onClick={handleClick} to="/contact">
             contact
           </Link>
         </li>
