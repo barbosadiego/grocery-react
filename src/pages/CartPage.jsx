69b6ecee05c0aabcac4d079d84b0d82e72a8c97e
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-14 16:39:46 -0300

    create cartpage component

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,11 @@
+import './CartPage.scss';
+
+const CartPage = () => {
+  return (
+    <section className="cart-page container">
+      <p>cart page</p>
+    </section>
+  );
+};
+
+export default CartPage;
