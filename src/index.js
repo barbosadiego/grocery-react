1ac526801c27f40e61008e66f01649e7f1074a97
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-16 11:21:45 -0300

    add cart context to app scope

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDom from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { GlobalProvider } from './contexts/GlobalContext';
+import { CartProvider } from './contexts/CartContext';
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GlobalProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
     </GlobalProvider>
   </StrictMode>,
 );
