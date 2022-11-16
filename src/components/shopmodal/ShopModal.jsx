feb383398654e4bc170aa6f5dc102cb8ac214b2c
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-16 11:22:41 -0300

    update shop modal

--- src/components/shopmodal/ShopModal.jsx
+++ src/components/shopmodal/ShopModal.jsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import { GlobalContext } from '../../contexts/GlobalContext';
 
 import './ShopModal.scss';
 
 export default function ShopModal() {
   const { state, handleShopModal, handleAppActive } = useContext(GlobalContext);
+  const { cart } = useContext(CartContext);
 
   function handleClose() {
     handleAppActive();
@@ -17,7 +19,7 @@ export default function ShopModal() {
         X
       </button>
       <div className="content">
-        {state.shopCart.map((item, index) => (
+        {cart.map((item, index) => (
           <div className="item" key={index}>
             <img
               className="img"
@@ -27,11 +29,10 @@ export default function ShopModal() {
               height={50}
             />
             <p className="title">{item.title}</p>
+            <p className="qty">Quantity: {item.qty}</p>
           </div>
         ))}
-        {state.shopCart.length === 0 && (
-          <p className="empty">No items in cart</p>
-        )}
+        {cart.length === 0 && <p className="empty">No items in cart</p>}
       </div>
     </div>
   );
